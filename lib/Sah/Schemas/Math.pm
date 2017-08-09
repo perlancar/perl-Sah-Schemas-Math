2bf9a669ffeccc97be5c0e7aa206abf2ab69d2c0
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-08-09 09:43:09 +0700

    Initial commit

---
+++ lib/Sah/Schemas/Math.pm
@@ -0,0 +1,18 @@
+package Sah::Schemas::Math;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Math-related Sah schemas
+
+=head1 DESCRIPTION
+
+Currently empty.
+
+
+=head1 SEE ALSO
+
+L<Sah::Schemas::Float>
+
+L<Sah::Schemas::Int>
