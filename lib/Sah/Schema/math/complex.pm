6d67f5c15c98f4f0dd0fb4fa51278fe5e6945d61
perlancar (on pc-home) <perlancar@gmail.com>
2021-12-11 07:20:00 +0700

    [doc] Fix description, tweak examples (make sure all invalid examples have explanation)

--- lib/Sah/Schema/math/complex.pm
+++ lib/Sah/Schema/math/complex.pm
@@ -1,29 +1,31 @@
 package Sah::Schema::math::complex;
 
+use strict;
+
+use Math::Complex ();
+
 # AUTHORITY
 # DATE
 # DIST
 # VERSION
 
-use Math::Complex ();
-
 our $schema = [obj => {
     summary   => 'Complex number',
    description => <<'_',
 
-See also `posfloat` for floats that are larger than 0.
+A <pm:Math::Complex> object, coercible from string in the form of "<a> + <b>i".
 
 _
     isa => 'Math::Complex',
     'x.perl.coerce_rules' => ['From_str::math_complex'],
 
     examples => [
-        {value=>'', valid=>0},
-        {value=>'abc', valid=>0},
+        {value=>'', valid=>0, summary=>"Empty string"},
+        {value=>'abc', valid=>0, summary=>"Not in the form of a+bi"},
         {value=>Math::Complex->make(5,6), valid=>1},
         {value=>"5 + 6i", valid=>1, validated_value=>Math::Complex->make(5, 6)},
     ],
-}, {}];
+}];
 
 1;
 # ABSTRACT:
